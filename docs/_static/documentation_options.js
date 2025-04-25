7027bc5010154ce940703e16cd8acac643380ecb
Tobias Block <tobias.block@nyumc.org>
2025-04-25 01:47:32 -0400

    Updated docs.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version 0.4.0-beta.4',
+    VERSION: 'Version 0.4.0-beta.6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
